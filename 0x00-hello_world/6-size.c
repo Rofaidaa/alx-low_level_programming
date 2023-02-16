ebbf4a202a76040bcdbc7842b25fd92c792be761
Rofaidaa <rofaidaa62@gmail.com>
2023-02-16 06:01:42 -0800

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - puts)("programming")
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+printf("Size of a char is=%i bytes\n",sizeof(char));
+printf("Size of a int is=%i bytes\n",sizeof(int));
+printf("Size of a long int is=%i bytes\n",sizeof(long int));
+printf("Size of a long long int is=% byte\n",sizeof(long long int));
+printf("Size of a float is=% bytes\n",sizeof(float));
+
+return (0);
+
+}
