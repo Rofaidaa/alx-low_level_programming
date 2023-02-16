831aa6b2a0d346e9eec07fae5693d53cec453c94
Rofaidaa <rofaidaa62@gmail.com>
2023-02-16 05:13:10 -0800

    6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - puts)("programming")
- *
- * Return: Always 0 (Success)
- */
+* main - puts)("programming")
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-         puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
-         return (0);
+return (0);
 
 }
