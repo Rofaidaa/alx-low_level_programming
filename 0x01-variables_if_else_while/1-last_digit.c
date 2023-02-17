a82e0b7fd52405bb74db97573eb048e185075c9a
Rofaidaa <rofaidaa62@gmail.com>
2023-02-17 10:16:20 -0800

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,11 +14,11 @@ if (x > 5)
 {
 printf("and is greater than 5");
 }
-if (x == 0)
+else if (x == 0)
 {
 printf("and is 0");
 }
-if (x < 6 && x != 0)
+else (x < 6 && x != 0)
 {
 printf("and is less than 6 and not 0");
 }
