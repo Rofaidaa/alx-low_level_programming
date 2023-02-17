76950965e641bd9eef1cb8b4f6bb8b1f2fe777bf
Rofaidaa <rofaidaa62@gmail.com>
2023-02-17 10:51:37 -0800

    1

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,16 +7,13 @@
  */
 int main(void)
 {
-	char letter;
-
-	letter = 'a';
-      while
-           (letter <= 'z'){
-		putchar(letter);
-           letter++;
-           }
-
-	putchar('\n');
-
-	return (0);
+char letter;
+letter = 'a';
+while
+(letter <= 'z') {
+putchar(letter);
+letter++;
+}
+putchar('\n');
+return (0);
 }
