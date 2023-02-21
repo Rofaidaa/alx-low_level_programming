285f1141b866c0d087e68526426df186f6488e38
Rofaidaa <rofaidaa62@gmail.com>
2023-02-21 11:11:12 -0800

    putchar task

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,25 +1,12 @@
 #include "main.h"
 
 /**
-  * print_alphabet_x10 - Make alphabet x10 times
-  *
-  * Return: void
-  */
-void print_alphabet_x10(void)
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	char c;
-	int i = 0;
-
-	while (i <= 9)
-	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
-
-		_putchar('\n');
-
-		i++;
-	}
+    print_alphabet_x10();
+    return (0);
 }
-
