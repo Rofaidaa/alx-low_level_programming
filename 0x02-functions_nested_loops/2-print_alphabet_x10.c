b2cca92459a3d5943ac024d726bf48d6c4a56ef4
Rofaidaa <rofaidaa62@gmail.com>
2023-02-21 11:20:20 -0800

    putchar task

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,8 +5,17 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-    print_alphabet_x10();
+	int count = 0;
+	char letter;
+
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
     return (0);
 }
