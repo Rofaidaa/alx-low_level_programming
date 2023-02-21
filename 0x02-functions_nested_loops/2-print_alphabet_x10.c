238385f98e27529c33500f14c3651e71747766cd
Rofaidaa <rofaidaa62@gmail.com>
2023-02-21 12:24:09 -0800

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,22 +2,21 @@
 
 /**
  * main - check the code.
- *
+ * 
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
-	int i = 0;
-	char c;
+int i = 0;
+char c;
 
-	while (i <= 9)
-	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
+while (i <= 9)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
 		}
-		_putchar('\n');
-		i++;
-	}
-
+_putchar('\n');
+i++;
+}
 }
