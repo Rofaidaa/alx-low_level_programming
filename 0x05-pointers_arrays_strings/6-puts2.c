5bb069b0504c47c7dd4ede77a7de85ebeef68af7
Rofaidaa <rofaidaa62@gmail.com>
2023-03-06 10:12:39 -0800

    pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,15 +11,14 @@ int longi = 0;
 int t = 0;
 char *y = str;
 int o;
-while (*y != '\0')
-(
-y++;
-longi++;
+ while (*y != '\0')
+{
+ y++;
 }
 t = longi - 1;
 for (o = 0 ; o <= t ; o++)
 {
-if (o % 2 == 0)
+ if (o % 2 == 0)
 {
 _putchar(str[o]);
 }
